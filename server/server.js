4b7b454111730e9e57b88142ccc72637a0049967
Travis Johnston <travisjohnston4588@icloud.com>
2019-11-24 14:08:56 -0600

    intial

--- server/server.js
+++ server/server.js
@@ -2,41 +2,46 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
+let history = [];
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post('/api/answer', (req, res)) => {
+app.post('/api/answer', (req, res) => {
     const value1 = parseFloat(req.body.value1);
     const value2 = parseFloat(req.body.value2);
     const operation = req.body.operation;
     let answer;
-    console.log(value1, value2, operation);
+    
+    if(operation == 'add') {
+        answer = value1 + value2;
+        res.send({answer:answer});
+    }
+    else if (operation == 'subtract') {
+        answer = value1 - value2;
+        res.send({answer:answer})
+    }
+    else if (operation == 'multiply') {
+        answer = value1 * value2;
+        res.send({ answer: answer })
+    }
+    else if (operation == 'divide') {
+        answer = value1 / value2;
+        res.send({ answer: answer })
+    }
+    const equationInputs = {value1, value2, operation, answer};
+    addEquation(equationInputs);
+    return;
+});
 
+function addEquation (equationInputs) {
+    history.push(equationInputs);
+};
 
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('api/history', (req, res) => {
+    res.send(history)
+});
 
 
 app.listen(PORT, () => {
